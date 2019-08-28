bc4088c58aa550e576660cf952e9858df53f3157
liuyao <ly@kingway.net>
2019-08-28 09:50:08 +0800

    引入eureka

---
+++ micro-service-eureka-7001/src/main/java/com/ly/springcloud/eureka/EurekaApplication7001.java
@@ -0,0 +1,20 @@
+package com.ly.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EnableEurekaServer 注解用于标注Eureka服务端，接受其他微服务的注册
+ *
+ * @author ashitaka
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication7001.class, args);
+    }
+
+}
